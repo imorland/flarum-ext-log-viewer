1093852dee948ccb3f9b9c1c2a7b67c71061a64f
Ian Morland <ian@morland.me>
2022-11-09 00:44:09 +0000

    chore: add show logfile test

--- tests/integration/api/ListLogFilesTest.php
+++ tests/integration/api/ListLogFilesTest.php
@@ -19,9 +19,6 @@ class ListLogFileTest extends TestCase
 {
     use RetrievesAuthorizedUsers;
 
-    public $filename = 'myFileName.txt';
-    public $content = 'my_test content --here';
-
     public function setUp(): void
     {
         parent::setUp();
@@ -54,6 +51,7 @@ class ListLogFileTest extends TestCase
         $data = Arr::get($json, 'data');
         $this->assertIsArray($json['data']);
         $this->assertEquals(1, count($data));
+        $this->assertEquals('logs', Arr::get($data[0], 'type'));
     }
 
     /**
@@ -77,6 +75,82 @@ class ListLogFileTest extends TestCase
     {
         $response = $this->send(
             $this->request('GET', '/api/logs')
+        );
+
+        $this->assertEquals(403, $response->getStatusCode());
+    }
+
+    /**
+     * @test
+     */
+    public function authorized_user_can_get_logfile()
+    {
+        $this->app()->getContainer()->make('log')->info('my !!!content');
+
+        $response = $this->send(
+            $this->request('GET', '/api/logs', [
+                'authenticatedAs' => 1,
+            ])
+        );
+
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $json = json_decode($response->getBody()->getContents(), true);
+        $data = Arr::get($json, 'data');
+        $logFileName = Arr::get($data[0], 'id');
+
+        $response = $this->send(
+            $this->request('GET', "/api/logs/$logFileName", [
+                'authenticatedAs' => 1,
+            ])
+        );
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $json = json_decode($response->getBody()->getContents(), true);
+        $data = Arr::get($json, 'data');
+
+        $this->assertIsArray($json['data']);
+        $this->assertEquals('logs', Arr::get($data, 'type'));
+        $this->assertStringContainsString('my !!!content', $data['attributes']['content']);
+    }
+
+    /**
+     * @test
+     */
+    public function unauthorized_user_cannot_get_logfile()
+    {
+        $this->app()->getContainer()->make('log')->info('my !!!content');
+
+        $response = $this->send(
+            $this->request('GET', '/api/logs', [
+                'authenticatedAs' => 1,
+            ])
+        );
+
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $json = json_decode($response->getBody()->getContents(), true);
+        $data = Arr::get($json, 'data');
+        $logFileName = Arr::get($data[0], 'id');
+
+        $response = $this->send(
+            $this->request('GET', "/api/logs/$logFileName", [
+                'authenticatedAs' => 2,
+            ])
+        );
+
+        $this->assertEquals(403, $response->getStatusCode());
+    }
+
+    /**
+     * @test
+     */
+    public function unauthorized_user_cannot_get_logfile_not_existing()
+    {
+        $response = $this->send(
+            $this->request('GET', '/api/logs/idontexist.log', [
+                'authenticatedAs' => 2,
+            ])
         );
 
         $this->assertEquals(403, $response->getStatusCode());
