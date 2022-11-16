b27591f49cb2a3e63bb0b8132a32d8eb6fe5d240
Ian Morland <ian@morland.me>
2022-11-16 17:30:26 +0000

    feat: purge logfiles after x days

--- js/src/admin/components/LogViewerPage.tsx
+++ js/src/admin/components/LogViewerPage.tsx
@@ -20,6 +20,19 @@ export default class LogViewerPage extends ExtensionPage {
             {/* Note to self: would be nice to show the filename here? */}
             <LogFileViewer className="LogViewerPage--fileContent" state={state} />
           </div>
+          <div className="LogViewerPage-settings">
+            <div className="Form-group">
+              {this.buildSettingComponent({
+                setting: 'ianm-log-viewer.purge-days',
+                type: 'number',
+                min: 2,
+                max: 365,
+                required: true,
+                label: app.translator.trans('ianm-log-viewer.admin.settings.purge-days'),
+              })}
+              {this.submitButton()}
+            </div>
+          </div>
         </div>
       </div>
     );
---
+++ src/Console/CleanupLogfilesCommand.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace IanM\LogViewer\Console;
+
+use Carbon\Carbon;
+use Flarum\Foundation\Paths;
+use Flarum\Settings\SettingsRepositoryInterface;
+use Illuminate\Console\Command;
+use Symfony\Component\Finder\Finder;
+use SplFileInfo;
+
+class CleanupLogfilesCommand extends Command
+{
+    protected $signature = 'logfiles:cleanup';
+    protected $description = 'Deletes logfiles older than {configured|90} days';
+
+    /**
+     * @var Paths
+     */
+    protected $paths;
+
+    /**
+     * @var Finder
+     */
+    protected $finder;
+
+    /**
+     * @var SettingsRepositoryInterface
+     */
+    protected $settings;
+
+    public function __construct(Paths $paths, Finder $finder, SettingsRepositoryInterface $settings)
+    {
+        parent::__construct();
+
+        $this->paths = $paths;
+        $this->finder = $finder;
+        $this->settings = $settings;
+    }
+
+    public function handle(): void
+    {
+        $logDir = $this->paths->storage.'/logs';
+
+        $days = $this->settings->get('ianm-log-viewer.purge-days');
+
+        if (!is_numeric($days)) {
+            $days = 90;
+        }
+
+        $searchDate = Carbon::now()->subDays($days);
+
+        $this->finder->files()->in($logDir)->date("< {$searchDate->toDateTimeString()}");
+
+        $this->info("Detected {$this->finder->count()} files in {$logDir} older than {$searchDate->toDateTimeString()}");
+
+        if ($this->finder->count() === 0) {
+            return;
+        }
+
+        $this->output->progressStart($this->finder->count());
+
+        $deleted = [];
+
+        foreach ($this->finder as $file) {
+            /** @var SplFileInfo $file */
+            $path = $file->getRealPath();
+            $deleted[] = $path;
+
+            unlink($path);
+
+            $this->output->progressAdvance();
+        }
+
+        $this->output->progressFinish();
+
+        foreach ($deleted as $path) {
+            $this->info("Deleted {$path}");
+        }
+    }
+}
