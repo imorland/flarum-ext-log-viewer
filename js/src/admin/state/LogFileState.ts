616b8f317d7414bc555b7a058aded45f19395ca5
IanM <ian@morland.me>
2025-12-17 18:56:39 +0000

    feat: download and delete

--- js/src/admin/state/LogFileState.ts
+++ js/src/admin/state/LogFileState.ts
@@ -1,8 +1,16 @@
 import app from 'flarum/admin/app';
 
 export default class LogFileState {
+  file: any;
+  onRefresh: (() => void) | null;
+
   constructor() {
     this.file = null;
+    this.onRefresh = null;
+  }
+
+  setRefreshCallback(callback: () => void) {
+    this.onRefresh = callback;
   }
 
   loadLogFile(filename: string) {
@@ -19,5 +27,35 @@ export default class LogFileState {
 
   getFile() {
     return this.file;
+  }
+
+  downloadFile(filename: string) {
+    const url = app.forum.attribute('apiUrl') + '/logs/download/' + filename + '?file=' + filename;
+    window.open(url, '_blank');
+  }
+
+  deleteFile(filename: string) {
+    if (!confirm(app.translator.trans('ianm-log-viewer.admin.viewer.confirm_delete'))) {
+      return Promise.resolve();
+    }
+
+    return app
+      .request({
+        method: 'DELETE',
+        url: app.forum.attribute('apiUrl') + '/logs/' + filename + '?file=' + filename,
+      })
+      .then(() => {
+        // Clear the currently selected file if it was deleted
+        if (this.file && this.file.data.attributes.fileName === filename) {
+          this.file = null;
+        }
+
+        // Refresh the file list
+        if (this.onRefresh) {
+          this.onRefresh();
+        }
+
+        m.redraw();
+      });
   }
 }
